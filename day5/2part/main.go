213ee2e84941b57e6293b3798492c0c897cba080
JoseH <josehm1999@gmail.com>
2023-12-17 20:47:38 -0500

    Fifth day progress

--- day5/2part/main.go
+++ day5/2part/main.go
@@ -14,6 +14,12 @@ func check(e error) {
 	}
 }
 
+type SeedValues struct {
+	src    int
+	dest   int
+	length int
+}
+
 func main() {
 	file, err := os.ReadFile("../input_day5.txt")
 	if err != nil {
@@ -22,8 +28,12 @@ func main() {
 	contents := strings.Split(string(file[:]), "\n\n")
 	seeds := strings.Split(strings.Split(contents[0], ": ")[1], " ")
 	fmt.Println(seeds)
-	for i := 1; i < len(contents)-1; i++ {
-		fmt.Println(contents[i])
+
+	for _, value := range seeds {
+		for i := 1; i < len(contents); i++ {
+			fmt.Println(value)
+			parseMap(contents[i])
+		}
 	}
 	// contents := strings.Split(file, "\n\n")
 	// sum := 0
@@ -34,15 +44,29 @@ func main() {
 
 }
 
-func createRange(line string) (int, int, int) {
+func createRange(line string) SeedValues {
 	items := strings.Split(line, " ")
 	dest, _ := strconv.Atoi(items[0])
 	src, _ := strconv.Atoi(items[0])
 	length, _ := strconv.Atoi(items[0])
-	return dest, src, length
+
+	seedValue := SeedValues{src, dest, length}
+	return seedValue
 }
 
-func parseMap(data []int)
+func parseMap(data string) {
+	// fmt.Println(data)
+    // var seed_values_arr =
+	lines := strings.Split(data, "\n")
+	src_to_dest := strings.Split(strings.Split(lines[0], " ")[0], "-")
+
+	src := src_to_dest[2]
+	dest := src_to_dest[0]
+	for i := 1; i < len(lines); i++ {
+	}
+	ranges := createRange(lines[])
+	fmt.Println(lines[0])
+}
 
 // func loopArraryOfArrays(wn_arr_of_arr [][]int, my_arr_arr [][]int, sum int) int {
 // 	curr_matches := 0

64f744c1f3a8da43ba694b7ee4d1ad2986bad36b
josehm1999 <josehm1999@gmail.com>
2023-12-20 18:01:54 -0500

    Fifth day - First part completed

--- day5/2part/main.go
+++ day5/2part/main.go
@@ -20,6 +20,11 @@ type SeedValues struct {
 	length int
 }
 
+type SeedRanges struct {
+	start  int
+	length int
+}
+
 func main() {
 	file, err := os.ReadFile("../input_day5.txt")
 	if err != nil {
@@ -27,27 +32,37 @@ func main() {
 	}
 	contents := strings.Split(string(file[:]), "\n\n")
 	seeds := strings.Split(strings.Split(contents[0], ": ")[1], " ")
-	// fmt.Println(seeds)
+	var seed_ranges_arr []SeedRanges
+
+	for i := 0; i < len(seeds)-1; i = i + 2 {
+		start, _ := strconv.Atoi(seeds[i])
+		length, _ := strconv.Atoi(seeds[i+1])
+		seed_ranges_arr = append(seed_ranges_arr, SeedRanges{start, length})
+	}
+	fmt.Println(seed_ranges_arr)
 	_, new_content := contents[0], contents[1:]
-	// fmt.Println(new_content[6])
-	for _, curr_seed := range seeds {
-		// curr_seed_int, _ := strconv.Atoi(curr_seed)
-		fmt.Println(curr_seed)
-		for i := 0; i < len(new_content); i++ {
-			// fmt.Println(new_content[i])
-			for _, val := range parseMap(new_content[i]) {
-				fmt.Println(val)
-				// 	// fmt.Println(curr_seed, val)
-				// 	if curr_seed_int >= val.src && curr_seed_int <= val.src+val.length-1 {
-				// 		curr_seed_int = curr_seed_int + val.dest - val.src
-				// 		fmt.Println(curr_seed, curr_seed_int)
-				// 	} else {
-				//
-				// 		fmt.Println(curr_seed, curr_seed_int)
-				// 	}
+
+	lowest_seed := 2147483647
+
+	for _, seed_range := range seed_ranges_arr {
+		for curr_seed := seed_range.start; curr_seed < seed_range.start+seed_range.length-1; curr_seed++ {
+			curr_seed_int := curr_seed
+			for i := 0; i < len(new_content); i++ {
+				for _, val := range parseMap(new_content[i]) {
+					if curr_seed_int >= val.src && curr_seed_int <= val.src+val.length-1 {
+						curr_seed_int = curr_seed_int + val.dest - val.src
+						break
+					}
+				}
+			}
+
+			if curr_seed_int < lowest_seed {
+				lowest_seed = curr_seed_int
 			}
 		}
 	}
+
+	fmt.Println(lowest_seed)
 }
 
 func createRange(line string) SeedValues {
@@ -60,76 +75,18 @@ func createRange(line string) SeedValues {
 }
 
 func parseMap(data string) []SeedValues {
-    fmt.Println(data)
 	var seed_values_arr []SeedValues
 	lines := strings.Split(data, "\n")
-	src_to_dest := strings.Split(strings.Split(lines[0], " ")[0], "-")
+	// src_to_dest := strings.Split(strings.Split(lines[0], " ")[0], "-")
 	_, new_line := lines[0], lines[1:]
-	src := src_to_dest[0]
-	dest := src_to_dest[2]
+	// src := src_to_dest[0]
+	// dest := src_to_dest[2]
 
 	for _, val := range new_line {
-		seed_values_arr = append(seed_values_arr, createRange(val))
+		if val != "" {
+			seed_values_arr = append(seed_values_arr, createRange(val))
+		}
 	}
-	fmt.Println(src, dest)
+	// fmt.Println(src, dest)
 	return seed_values_arr
 }
-
-// func loopArraryOfArrays(wn_arr_of_arr [][]int, my_arr_arr [][]int, sum int) int {
-// 	curr_matches := 0
-// 	// fmt.Println("Nuevo bucle1")
-// 	for i, wm_v := range wn_arr_of_arr {
-// 		for j, my_v := range my_arr_arr {
-// 			if i == j {
-// 				curr_matches = findMatches(wm_v, my_v)
-// 			}
-// 		}
-//
-// 		if curr_matches > 0 {
-// 			new_wn_arr_of_arr := wn_arr_of_arr[i+1 : i+curr_matches+1]
-// 			new_my_arr_of_arr := my_arr_arr[i+1 : i+curr_matches+1]
-// 			sum += loopArraryOfArrays(new_wn_arr_of_arr, new_my_arr_of_arr, curr_matches)
-// 		}
-// 	}
-// 	return sum
-// }
-//
-// func findMatches(wn_arr []int, my_arr []int) int {
-// 	num_ocurr := 0
-// 	for _, v := range wn_arr {
-// 		for _, my_v := range my_arr {
-// 			if v == my_v {
-// 				num_ocurr++
-// 			}
-// 		}
-// 	}
-// 	return num_ocurr
-// }
-//
-// func extractNumbers(s string) ([]int, []int) {
-// 	// runes := []rune(s)
-// 	var wn []int
-// 	var myn []int
-//
-// 	split_at_colon := strings.Split(s, ":")
-// 	// fmt.Println(split_at_colon[1])
-//
-// 	split_at_vertical_bar := strings.Split(split_at_colon[1], "|")
-// 	split_at_space_wn := strings.Split(strings.Trim(split_at_vertical_bar[0], " "), " ")
-// 	split_at_space_myn := strings.Split(strings.Trim(split_at_vertical_bar[1], " "), " ")
-//
-// 	for _, v := range split_at_space_wn {
-// 		curr_number, err := strconv.Atoi(v)
-// 		if err == nil {
-// 			wn = append(wn, curr_number)
-// 		}
-// 	}
-//
-// 	for _, v := range split_at_space_myn {
-// 		curr_number, err := strconv.Atoi(v)
-// 		if err == nil {
-// 			myn = append(myn, curr_number)
-// 		}
-// 	}
-// 	return wn, myn
-// }

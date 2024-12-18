eee74df35dd0d224d1bf8ed4a169313a788c2c94
JoseH <josehm1999@gmail.com>
2024-12-18 01:42:41 -0500

    Day6 part2 progress

--- 2024/day6/main.go
+++ 2024/day6/main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"log"
 	"os"
-	"strconv"
 	"strings"
 )
 
 func main() {
-	part1()
+	part2()
 }
 
 type Point struct {
@@ -19,7 +18,6 @@ type Point struct {
 type Horientation struct {
 	horientation string
 	check        bool
-	counter      int
 }
 
 func part1() {
@@ -58,28 +56,9 @@ func part1() {
 	counter := 0
 	horientation := "up"
 
-	result := walk(maze, Point{x: guard_x, y: guard_y}, &horientation, &counter, &seen)
+	walk(maze, Point{x: guard_x, y: guard_y}, &horientation, &counter, &seen)
 
-	println(result)
 	println(counter)
-	//Base case
-	//1.- Its an obstacle
-	//2.- Off the map end
-	//3.- Have we been here
-	//4.- Each new tile needs to sum to a counter
-
-	//Recurse
-	//1.- Only go forward
-	//2.- Keep track of horientation
-	//3.- If hits a wall go right depending horientation
-
-	//Pre
-	//Recurse
-
-	// if (walk)
-	// walk()
-	//Post
-	// println(current_guard_position, temp)
 }
 
 func changeHorientation(actualHorientation string) string {
@@ -151,6 +130,100 @@ func walk(maze [][]string, current Point, horientation *string, counter *int, se
 	return false
 }
 
+func walk2(maze [][]string, current Point, horientation *string, counter *int, seen *[][]Horientation) bool {
+
+	// println("Cuantas veces entra", *counter)
+	dirs := map[string][2]int{
+		"up":    {-1, 0},
+		"down":  {1, 0},
+		"left":  {0, -1},
+		"right": {0, 1},
+	}
+
+	// Se salio del laberinto
+	if current.x < 0 || current.x >= len(maze[0]) || current.y < 0 || current.y >= len(maze)-1 {
+		return false
+	}
+
+	// En una pared
+	if maze[current.y][current.x] == "#" {
+		switch *horientation {
+		case "up":
+			current.x = current.x + dirs["down"][1]
+			current.y = current.y + dirs["down"][0]
+		case "right":
+			current.x = current.x + dirs["left"][1]
+			current.y = current.y + dirs["left"][0]
+		case "down":
+			current.x = current.x + dirs["up"][1]
+			current.y = current.y + dirs["up"][0]
+		case "left":
+			current.x = current.x + dirs["right"][1]
+			current.y = current.y + dirs["right"][0]
+		}
+
+		*horientation = changeHorientation(*horientation)
+		// println("Regresa una posicion")
+	}
+
+	// Si no se ha visto aumentar el contador
+	// if (*seen)[current.y][current.x].check && *horientation == (*seen)[current.y][current.x].horientation {
+	// 	return true
+	// }
+
+	// println(*horientation, (*seen)[current.y][current.x].horientation)
+	// if (*seen)[current.y][current.x].check && (*seen)[current.y][current.x].horientation != *horientation {
+	// 	// *counter++
+	// 	// Dependiendo de la horientacion validar a los lados para determinar si ha pasado por ahi
+	//
+	// 	println(current.y, current.x, *horientation)
+	// 	if maze[current.y][current.x] != "^" {
+	//
+	// 		switch *horientation {
+	// 		case "up":
+	// 			tmp_x := current.x + dirs["right"][1]
+	// 			tmp_y := current.y + dirs["right"][0]
+	// 			if (*seen)[tmp_y][tmp_x].check {
+	// 				*counter++
+	// 			}
+	// 		case "right":
+	// 			tmp_x := current.x + dirs["down"][1]
+	// 			tmp_y := current.y + dirs["down"][0]
+	//
+	// 			if (*seen)[tmp_y][tmp_x].check {
+	// 				*counter++
+	// 			}
+	// 		case "down":
+	// 			tmp_x := current.x + dirs["down"][1]
+	// 			tmp_y := current.y + dirs["down"][0]
+	//
+	// 			if (*seen)[tmp_y][tmp_x].check {
+	// 				*counter++
+	// 			}
+	// 		case "left":
+	// 			tmp_x := current.x + dirs["down"][1]
+	// 			tmp_y := current.y + dirs["down"][0]
+	//
+	// 			if (*seen)[tmp_y][tmp_x].check {
+	// 				*counter++
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	// Actualizar posicion a visto
+	(*seen)[current.y][current.x].check = true
+	(*seen)[current.y][current.x].horientation = *horientation
+
+	// Como aun no ha salido entrar en bucle
+	if (walk2(maze, Point{x: current.x + dirs[*horientation][1],
+		y: current.y + dirs[*horientation][0]}, horientation, counter, seen)) {
+		return true
+	}
+
+	return true
+}
+
 func part2() {
 
 	data, err := os.ReadFile("./input.txt")
@@ -161,94 +234,35 @@ func part2() {
 
 	file := string(data)
 
-	// line := 0
+	current_guard_position := strings.Index(file, "^")
+	temp := strings.Split(file, "\n")
 
-	temp := strings.Split(file, "\n\n")
+	// println(file)
 
-	rulesArr := strings.Split(temp[0], "\n")
-	pagesToUpdate := strings.Split(temp[1], "\n")
+	maxNumberOfMovements := (len(temp) - 1) * len(temp[0])
+	var maze [][]string
+	var seen [][]Horientation
 
-	sum := 0
-	// rules := make(map[int]int)
-
-	// orderedPagesArr := make([]map[string]int, len(pagesToUpdate)-1)
-
-	for i := 0; i < len(pagesToUpdate)-1; i++ {
-		numArr := strings.Split(pagesToUpdate[i], ",")
-		orderedPages := make(map[string]int)
-
-		// currentMiddleNum := ""
-		for j, numToUpdate := range numArr {
-			orderedPages[numToUpdate] = j
-			// if (len(numArr)-1)/2 == j {
-			// 	currentMiddleNum = numToUpdate
-			// }
-		}
-
-		isValid := true
-		for _, v := range rulesArr {
-			rulesLevels := strings.Split(v, "|")
-			lowerLevel := rulesLevels[0]
-			upperLevel := rulesLevels[1]
-
-			// println(lowerLevel, upperLevel)
-			lowerBoundIndex, existsLower := orderedPages[lowerLevel]
-			upperBoundIndex, existsUpper := orderedPages[upperLevel]
-
-			if !existsLower || !existsUpper {
-				continue
-			}
-
-			if lowerBoundIndex > upperBoundIndex {
-				isValid = false
-			}
-
-		}
-
-		// if isValid {
-		// 	value, _ := strconv.Atoi(currentMiddleNum)
-		// 	sum += value
-		// }
-
-		if !isValid {
-
-			// println("New Line")
-			for _, v := range rulesArr {
-				rulesLevels := strings.Split(v, "|")
-				lowerLevel := rulesLevels[0]
-				upperLevel := rulesLevels[1]
-
-				lowerBoundIndex, existsLower := orderedPages[lowerLevel]
-				upperBoundIndex, existsUpper := orderedPages[upperLevel]
-
-				if !existsLower || !existsUpper {
-					continue
-				}
-
-				if lowerBoundIndex > upperBoundIndex {
-					orderedPages[lowerLevel] = upperBoundIndex
-					orderedPages[upperLevel] = lowerBoundIndex
-					numArr[upperBoundIndex] = lowerLevel
-					numArr[lowerBoundIndex] = upperLevel
-				}
-
-				// println(lowerBoundIndex, lowerLevel)
-				// println(upperBoundIndex, upperLevel)
-				//
-				// println("------")
-			}
-
-			test := numArr[(len(numArr)-1)/2]
-
-			// for _, v := range numArr {
-			// 	println(v)
-			// }
-			// println(test)
-			value, _ := strconv.Atoi(test)
-			// println(test)
-			sum += value
-		}
+	println(maxNumberOfMovements)
+	for _, v := range temp {
+		stringArr := strings.Split(v, "")
+		maze = append(maze, stringArr)
+		tmp_seen := make([]Horientation, len(stringArr))
+		// tmp_path := make([]Point, len(stringArr))
+		seen = append(seen, tmp_seen)
+		// path = append(path, tmp_path)
 	}
 
-	println(sum)
+	guard_y := current_guard_position / len(temp)
+	guard_x := current_guard_position % (len(temp))
+
+	// println(maze[guard_y][guard_x])
+	// current_guard_line := len(temp)
+	// println("Empieza en ", guard_x, guard_y)
+	counter := 0
+	horientation := "up"
+
+	walk2(maze, Point{x: guard_x, y: guard_y}, &horientation, &counter, &seen)
+
+	println(counter)
 }

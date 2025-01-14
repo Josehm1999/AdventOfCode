11878b9ef3424306d9f210076e57ed06831e1dd3
josehm1999 <josehm1999@gmail.com>
2025-01-14 17:59:28 -0500

    Day9 progress

--- 2024/day9/main.go
+++ 2024/day9/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -24,7 +25,24 @@ func part1() {
 	temp := strings.Split(file, "\n")
 
 	values_arr := strings.Split(temp[0], "")
-	fmt.Println(slidingWindow(5, values_arr))
+
+	var disk_map_uncompressed []string
+	for i, val := range values_arr {
+		parsed_val, _ := strconv.Atoi(val)
+		if i%2 == 0 {
+			fmt.Println("This is multiple of two", i, parsed_val)
+		} else {
+			temp_free_space := make([]string, parsed_val)
+
+			for tpf := range temp_free_space {
+				temp_free_space[tpf] = "."
+			}
+			fmt.Println("This is the amount of free space", i, parsed_val)
+			disk_map_uncompressed = append(disk_map_uncompressed, temp_free_space...)
+		}
+	}
+
+	fmt.Println(slidingWindow(5, values_arr), disk_map_uncompressed)
 
 }
 
@@ -36,6 +54,7 @@ func slidingWindow(size int, input []string) [][]string {
 	r := make([][]string, 0, len(input)-size+1)
 
 	for i, j := 0, size; j <= len(input); i, j = i+1, j+1 {
+
 		r = append(r, input[i:j])
 	}
 

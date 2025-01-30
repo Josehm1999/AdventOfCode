b053ff0b6d580a3fe596f94e86f2b1d26b44a3dd
josehm1999 <josehm1999@gmail.com>
2025-01-30 16:46:14 -0500

    Day9: 1st part completed, 2nd progress

--- 2024/day9/main.go
+++ 2024/day9/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -12,7 +14,8 @@ func main() {
 	part1()
 }
 
-type FreeSpace struct {
+type DiskSpace struct {
+	value string
 	start int
 	end   int
 }
@@ -30,7 +33,8 @@ func part1() {
 	temp := strings.Split(file, "\n")
 
 	values_arr := strings.Split(temp[0], "")
-
+	var free_space_record []DiskSpace
+	var file_space_record []DiskSpace
 	var disk_map_uncompressed []string
 	for i, val := range values_arr {
 		parsed_val, _ := strconv.Atoi(val)
@@ -41,6 +45,8 @@ func part1() {
 				str_val := strconv.Itoa(i / 2)
 				temp_file[tf] = str_val
 			}
+
+			file_space_record = append(file_space_record, DiskSpace{value: temp_file[0], start: len(disk_map_uncompressed), end: len(disk_map_uncompressed) + parsed_val})
 			disk_map_uncompressed = append(disk_map_uncompressed, temp_file...)
 		} else {
 			if parsed_val == 0 {
@@ -50,109 +56,87 @@ func part1() {
 			for tpf := range temp_free_space {
 				temp_free_space[tpf] = "."
 			}
+
+			free_space_record = append(free_space_record, DiskSpace{value: "", start: len(disk_map_uncompressed), end: len(disk_map_uncompressed) + parsed_val})
 			disk_map_uncompressed = append(disk_map_uncompressed, temp_free_space...)
 		}
 	}
 
 	fmt.Println((disk_map_uncompressed))
-	for i, val := range disk_map_uncompressed {
-		// for i := 0; i < len(disk_map_uncompressed)-1; i++ {
-		if val == "." {
-			possible_num := disk_map_uncompressed[len(disk_map_uncompressed)-1]
-			disk_map_uncompressed[len(disk_map_uncompressed)-1] = ""
-			disk_map_uncompressed = disk_map_uncompressed[:len(disk_map_uncompressed)-1]
+	// for i, val := range disk_map_uncompressed {
 
-			for possible_num == "." {
-				possible_num = disk_map_uncompressed[len(disk_map_uncompressed)-1]
-				disk_map_uncompressed[len(disk_map_uncompressed)-1] = ""
-				disk_map_uncompressed = disk_map_uncompressed[:len(disk_map_uncompressed)-1]
-			}
+	freqCmp := func(a, b DiskSpace) int {
+		return cmp.Compare(b.start, a.start)
+	}
+	slices.SortFunc(file_space_record, freqCmp)
 
-			if i <= len(disk_map_uncompressed) {
-				disk_map_uncompressed[i] = possible_num
-			}
+	for i := range file_space_record {
+		for j := range free_space_record {
+			fmt.Println(file_space_record[i], free_space_record[j])
 		}
 	}
+	// for i := range free_space_record {
+	// 	for _, file_space_range := range file_space_record {
+	//
+	// 		// Avoid looking for file spaces before free spaces
+	// 		if free_space_record[i].start < file_space_range.start {
+	// 			// Check if file space fits the free space
+	// 			if free_space_record[i].end-free_space_record[i].start >= file_space_range.end-file_space_range.start {
+	// 				fmt.Println("Matches", free_space_record[i], file_space_range)
+	// 				file_space := disk_map_uncompressed[file_space_range.start:file_space_range.end]
+	//
+	// 				//Copy file spaces into the free spaces
+	// 				copy(disk_map_uncompressed[free_space_record[i].start:free_space_record[i].start+file_space_range.end-file_space_range.start], file_space)
+	//
+	// 				//Replace file spaces with "."
+	// 				for k := file_space_range.start; k < file_space_range.end; k++ {
+	// 					disk_map_uncompressed[k] = "."
+	// 				}
+	//
+	// 				if free_space_record[i].end-free_space_record[i].start > file_space_range.end-file_space_range.start {
+	// 					free_space_record[i].start += file_space_range.end - file_space_range.start
+	// 					fmt.Println(free_space_record[i])
+	// 				}
+	//
+	// 				if free_space_record[i].end-free_space_record[i].start == file_space_range.end-file_space_range.start {
+	// 					break
+	// 				}
+	// 				file_space_record = file_space_record[1:]
+	// 				fmt.Println((disk_map_uncompressed))
+	//
+	// 				// break
+	// 			}
+	// 		}
+	// 	}
+	// 	// fmt.Println(i, val)
+	// }
+	fmt.Println((disk_map_uncompressed))
+	// Part1
+	// for i := 0; i < len(disk_map_uncompressed)-1; i++ {
+	// 	if disk_map_uncompressed[i] == "." {
+	// possible_num := disk_map_uncompressed[len(disk_map_uncompressed)-1]
+	// disk_map_uncompressed = disk_map_uncompressed[:len(disk_map_uncompressed)-1]
+	//
+	// for possible_num == "." && len(disk_map_uncompressed) > i {
+	// 	possible_num = disk_map_uncompressed[len(disk_map_uncompressed)-1]
+	// 	disk_map_uncompressed = disk_map_uncompressed[:len(disk_map_uncompressed)-1]
+	// }
+	//
+	// if len(disk_map_uncompressed) > i {
+	// 	disk_map_uncompressed[i] = possible_num
+	// }
+	// 	}
+	// }
 
 	checksum := 0
 	for k, value := range disk_map_uncompressed {
-		parsed_value, _ := strconv.Atoi(value)
-		checksum += k * parsed_value
+		parsed_value, error := strconv.Atoi(value)
+		if error == nil {
+			checksum += k * parsed_value
+		}
 	}
 
 	fmt.Println(checksum)
-	// fmt.Println((disk_map_uncompressed))
-	// endpoint := len(disk_map_uncompressed)
-	// for _, val := range free_space_record {
-	// 	amount_to_move := val.end - val.start
-	// 	// possible_files_to_move := disk_map_uncompressed[endpoint-amount_to_move : endpoint]
-	//
-	// 	// fmt.Println("New line")
-	// 	for i := endpoint - 1; i >= endpoint-amount_to_move; i-- {
-	// 		if disk_map_uncompressed[i] == "." {
-	//
-	// 			i = i - 1
-	// 			// fmt.Println(disk_map_uncompressed[i])
-	// 			endpoint = endpoint - 1
-	// 			// i--
-	// 		}
-	// 		// fmt.Println(disk_map_uncompressed[i])
-	// 		disk_map_uncompressed[val.start] = disk_map_uncompressed[i]
-	// 		disk_map_uncompressed[i] = "."
-	// 		val.start = val.start + 1
-	// 	}
-	//
-	// 	// Get to the first point possible "." then iterate from there until the end
-	// 	// Capture next possible position to move a file if its not possible and has reached the end means its done
-	//
-	// 	// for _, val := range possible_files_to_move {
-	// 	// 	if val == "." {
-	// 	// 		possible_files_to_move = disk_map_uncompressed[endpoint-amount_to_move-1 : endpoint-1]
-	// 	// 	}
-	// 	// }
-	// 	// for i, value := range possible_files_to_move {
-	// 	// 	disk_map_uncompressed[val.start+i] = value
-	// 	// 	disk_map_uncompressed[endpoint-amount_to_move+i] = "."
-	// 	// }
-	//
-	// 	// fmt.Println(amount_to_move, possible_files_to_move)
-	// 	endpoint -= amount_to_move
-	//
-	// }
-
-	// first_pos_dot := 0
-	// for i, ffdot := range disk_map_uncompressed {
-	// 	if ffdot == "." {
-	// 		first_pos_dot = i
-	// 		break
-	// 	}
-	// }
-	//
-	// next_available_dot := first_pos_dot
-	//
-	// for j := first_pos_dot; j < len(disk_map_uncompressed); j++ {
-	// 	if disk_map_uncompressed[j] != "." {
-	// 		disk_map_uncompressed[next_available_dot] = disk_map_uncompressed[j]
-	// 		disk_map_uncompressed[j] = "."
-	//
-	// 		next_available_dot++
-	// 		// fmt.Println(disk_map_uncompressed[j])
-	// 	}
-	// 	// fmt.Println(disk_map_uncompressed[j], next_available_dot)
-	// }
-	//
-	// checksum := 0
-	// for k, value := range disk_map_uncompressed {
-	// 	if value == "." {
-	// 		break
-	// 	}
-	//
-	// 	parsed_value, _ := strconv.Atoi(value)
-	//
-	// 	checksum += k * parsed_value
-	// }
-
-	// fmt.Println(checksum)
 }
 
 func slidingWindow(size int, input []string) [][]string {

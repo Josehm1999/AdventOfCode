ad22d57ff92cefb10114e44d1a252082f8da9484
josehm1999 <josehm1999@gmail.com>
2025-02-19 18:03:06 -0500

    Day10 go completed

--- 2024/day11/main.go
+++ 2024/day11/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	part2()
+	part1()
 }
 
 type Point struct {
@@ -29,28 +29,33 @@ func part1() {
 
 	temp := strings.Split(file, "\n")
 
-	var start_points []Point
-	var topo_map [][]string
+	num_arr := strings.Split(temp[0], " ")
 
-	for i, v := range temp {
-		stringArr := strings.Split(v, "")
-		for j, val := range stringArr {
-			if val == "0" {
-				start_points = append(start_points, Point{row: i, col: j})
+	blink_num := 6
+
+	for i := 0; i < blink_num; i++ {
+		for j := range num_arr {
+
+			if num_arr[j] == "0" {
+				num_arr[j] = "1"
+			} else if len(num_arr[j])%2 == 0 {
+				halfway := len(num_arr[j]) / 2
+				left_stone := num_arr[j][:halfway]
+				right_stone := num_arr[j][halfway:]
+
+				num_arr[j] = left_stone
+				fmt.Println("Tiene un numero par de caracteres", num_arr[j], halfway, left_stone, right_stone)
+				test := append([]string{right_stone}, num_arr[j:]...)
+				num_arr = append(num_arr[:j], test...)
+			} else {
+				num_int, _ := strconv.Atoi(num_arr[j])
+
+				num_arr[j] = strconv.Itoa(num_int * 2024)
 			}
+			fmt.Println(i, j)
 		}
-		topo_map = append(topo_map, stringArr)
 	}
-
-	counter := 0
-	// Start from every 0 position
-
-	for _, start := range start_points {
-		result := walk(topo_map, start)
-		counter += len(removeDuplicate(result))
-	}
-
-	fmt.Println(counter)
+	// fmt.Println(num_arr)
 }
 
 func part2() {

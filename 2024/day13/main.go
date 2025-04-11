0b6bbe8cbf90ccad8b2489c7751473fc3d958021
josehm1999 <josehm1999@gmail.com>
2025-04-10 19:25:37 -0500

    Day13 progress

--- 2024/day13/main.go
+++ 2024/day13/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -35,9 +36,32 @@ func part1() {
 		individual_lines := strings.Split(crawl_machine, "\n")
 		// fmt.Println(individual_lines)
 
-		fmt.Println(individual_lines[0], individual_lines[1], individual_lines[2])
+		button_a_string := individual_lines[0]
+		button_b_string := individual_lines[1]
+		prize_string := individual_lines[2]
+
+		var button_a_arr [2]int
+		var button_b_arr [2]int
+		var prize_arr [2]int
+		for i, v := range strings.Split(button_a_string, ",") {
+			plus_symbol_idx := strings.Index(v, "+")
+			a_value, _ := strconv.Atoi(v[plus_symbol_idx+1:])
+			button_a_arr[i] = a_value
+		}
+		for i, v := range strings.Split(button_b_string, ",") {
+			plus_symbol_idx := strings.Index(v, "+")
+			b_value, _ := strconv.Atoi(v[plus_symbol_idx+1:])
+			button_b_arr[i] = b_value
+		}
+		for i, v := range strings.Split(prize_string, ",") {
+			equal_symbol_idx := strings.Index(v, "=")
+			prize_value, _ := strconv.Atoi(v[equal_symbol_idx+1:])
+			prize_arr[i] = prize_value
+		}
+
+		// fmt.Println(button_a_arr[0]%button_b_arr[0], button_b_arr[0])
+		fmt.Println(button_a_arr, button_b_arr, prize_arr)
 	}
-	// fmt.Println(temp[0])
 }
 
 func part2() {

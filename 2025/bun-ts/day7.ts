70685f754905f7b5a44098bb90a19bcebc2be5b5
josehm1999 <josehm1999@gmail.com>
2025-12-12 14:08:40 -0500

    2025 part 7 completed

--- 2025/bun-ts/day7.ts
+++ 2025/bun-ts/day7.ts
@@ -43,65 +43,57 @@ async function part1() {
   console.log(rowsSplittedLetters, counter);
 }
 
+interface Point {
+  row: number;
+  col: number;
+}
+
 // part1();
 async function part2() {
-  const content = await Bun.file("./test_day7_example.txt").text();
+  const content = await Bun.file("./test_day7.txt").text();
   const rows = content.split("\n").filter((ifilter) => ifilter !== "");
   const rowsSplittedLetters: string[][] = [];
 
   rows.forEach((r) => rowsSplittedLetters.push(r.split("")));
 
-  let counter = 0;
-  for (let i = 0; i < rowsSplittedLetters.length; i++) {
-    for (let j = 0; j < rowsSplittedLetters[i].length; j++) {
-      // console.log(rowsSplittedLetters[i][j]);
-      const currLetter = rowsSplittedLetters[i][j];
-      if (currLetter == "S") {
-        if (rowsSplittedLetters[i + 1][j] == ".") {
-          rowsSplittedLetters[i + 1][j] = "|";
-        }
-
-        if (rowsSplittedLetters[i + 1][j] == "^") {
-          rowsSplittedLetters[i + 1][j - 1] = "|";
-          rowsSplittedLetters[i + 1][j + 1] = "|";
-        }
-      }
-
-      if (currLetter == "^") {
-        if (rowsSplittedLetters[i - 1][j] == "|") {
-          rowsSplittedLetters[i][j - 1] = "|";
-          rowsSplittedLetters[i][j + 1] = "|";
-        }
-      }
-
-      if (currLetter == ".") {
-        if (i > 0 && rowsSplittedLetters[i - 1][j] == "|") {
-          rowsSplittedLetters[i][j] = "|";
-        }
-      }
+  let startingPoint: Point | null = null;
+  for (let j = 0; j < rowsSplittedLetters[0].length; j++) {
+    const currLetter = rowsSplittedLetters[0][j];
+    if (currLetter == "S") {
+      startingPoint = { row: 0, col: j };
+      break;
     }
   }
 
-  for (let i = 0; i < rowsSplittedLetters.length; i++) {
-    console.log(rowsSplittedLetters[i].join(""));
-    for (let j = 0; j < rowsSplittedLetters[i].length; j++) {
-      if (
-        // i != 2 &&
-        rowsSplittedLetters[i][j] == "^" &&
-        rowsSplittedLetters[i - 1][j] == "|"
-      ) {
-        if (rowsSplittedLetters[i][j - 1] == "|") {
-          counter++;
-        }
+  function recurse(point: Point, memo?: Map<string, number>) {
+    memo = memo || new Map();
 
-        if (rowsSplittedLetters[i][j + 1] == "|") {
-          counter++;
-        }
-      }
+    const key1 = `${point.row}-${point.col}`;
+    if (memo.has(key1)) {
+      return memo.get(key1);
+    }
+
+    if (point.row > rowsSplittedLetters.length - 1) {
+      return 1;
+    }
+
+    if (
+      rowsSplittedLetters[point.row][point.col] == "." ||
+      rowsSplittedLetters[point.row][point.col] == "S"
+    ) {
+      return recurse({ row: point.row + 1, col: point.col }, memo);
+    } else if (rowsSplittedLetters[point.row][point.col] == "^") {
+      const val =
+        recurse({ row: point.row, col: point.col - 1 }, memo) +
+        recurse({ row: point.row, col: point.col + 1 }, memo);
+      const key2 = `${point.row}-${point.col}`;
+      memo.set(key2, val);
+      return val;
     }
   }
-
-  // console.log(rowsSplittedLetters, counter);
+  // console.log(startingPoint);
+  const counter = recurse(startingPoint!);
+  console.log(counter);
 }
 
 part2();

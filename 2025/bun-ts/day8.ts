a82e093779bb38892cde5aeb0a0d02f7d891505a
JoseH <josehm1999@gmail.com>
2025-12-15 01:16:15 -0500

    2025 day 8 completed part 1 and part 2 progress

--- 2025/bun-ts/day8.ts
+++ 2025/bun-ts/day8.ts
@@ -25,11 +25,64 @@ async function part1() {
     });
   });
 
-  // const test = Math.sqrt(
-  //   Math.pow(rowsSplittedLetters[0].x - rowsSplittedLetters[1].x, 2) +
-  //     Math.pow(rowsSplittedLetters[0].y - rowsSplittedLetters[1].y, 2) +
-  //     Math.pow(rowsSplittedLetters[0].z - rowsSplittedLetters[1].z, 2),
-  // );
+  const currentDistanceArr: Distance[] = [];
+  for (let i = 0; i < rowsSplittedLetters.length; i++) {
+    for (let j = i + 1; j < rowsSplittedLetters.length; j++) {
+      const distance = Math.sqrt(
+        Math.pow(rowsSplittedLetters[i].x - rowsSplittedLetters[j].x, 2) +
+          Math.pow(rowsSplittedLetters[i].y - rowsSplittedLetters[j].y, 2) +
+          Math.pow(rowsSplittedLetters[i].z - rowsSplittedLetters[j].z, 2),
+      );
+
+      currentDistanceArr.push({ startIdx: i, endIdx: j, distance });
+    }
+  }
+  const test = currentDistanceArr.sort((a, b) => b.distance - a.distance);
+  console.log(test);
+  const top10 = currentDistanceArr
+    .sort((a, b) => a.distance - b.distance)
+    .slice(0, 10);
+  // .sort((a, b) => a.startIdx - b.startIdx);
+  const arrTop10: number[][] = [];
+  top10.forEach((a) => arrTop10.push([a.startIdx, a.endIdx]));
+
+  for (let i = 0; i < arrTop10.length; i++) {
+    for (let k = i + 1; k < arrTop10.length; k++) {
+      const intersection = arrTop10[k].filter((x) => arrTop10[i].includes(x));
+      if (intersection!.length > 0) {
+        const difference = arrTop10[k]?.filter((x) => !arrTop10[i].includes(x));
+
+        if (difference!.length > 0) {
+          arrTop10[i].push(...difference!);
+          arrTop10.splice(k, 1);
+          i = 0;
+        } else {
+          arrTop10.splice(k, 1);
+          i = 0;
+        }
+      }
+    }
+  }
+
+  arrTop10.sort((a, b) => b.length - a.length);
+  console.log(arrTop10[0].length * arrTop10[1].length * arrTop10[2].length);
+}
+
+// part1();
+async function part2() {
+  const content = await Bun.file("./test_day8_example.txt").text();
+  const rows = content.split("\n").filter((ifilter) => ifilter !== "");
+  const rowsSplittedLetters: JBox[] = [];
+
+  rows.forEach((r) => {
+    const coords = r.split(",");
+
+    rowsSplittedLetters.push({
+      x: parseInt(coords[0]),
+      y: parseInt(coords[1]),
+      z: parseInt(coords[2]),
+    });
+  });
 
   const currentDistanceArr: Distance[] = [];
   for (let i = 0; i < rowsSplittedLetters.length; i++) {
@@ -46,86 +99,33 @@ async function part1() {
   const top10 = currentDistanceArr
     .sort((a, b) => a.distance - b.distance)
     .slice(0, 10);
-  // .sort((a, b) => a.startIdx - b.startIdx);
-  //
+  const arrTop10: number[][] = [];
 
-  // console.log(
-  //   currentDistanceArr.sort((a, b) => a.distance - b.distance).slice(0, 10),
-  // );
-  const jukeConnectionsIdxs: number[][] = [];
-  for (let i = 0; i < top10.length; i++) {
-    const localJukeIdxs: number[] = [];
-    for (let j = i + 1; j < top10.length; j++) {
-      if (
-        top10[i].startIdx == top10[j].startIdx ||
-        top10[i].endIdx == top10[j].endIdx ||
-        top10[i].startIdx == top10[j].endIdx ||
-        top10[i].endIdx == top10[j].startIdx
-      ) {
-        // console.log(2, top10[i], top10[j]);
-        localJukeIdxs.push(
-          top10[i].startIdx,
-          top10[i].endIdx,
-          top10[j].startIdx,
-          top10[j].endIdx,
-        );
+  top10.forEach((a) => arrTop10.push([a.startIdx, a.endIdx]));
+  for (let i = 0; i < arrTop10.length; i++) {
+    for (let k = i + 1; k < arrTop10.length; k++) {
+      const intersection = arrTop10[k].filter((x) => arrTop10[i].includes(x));
+      if (intersection!.length > 0) {
+        const difference = arrTop10[k]?.filter((x) => !arrTop10[i].includes(x));
+        if (difference!.length > 0) {
+          arrTop10[i].push(...difference);
+          arrTop10.splice(k, 1);
+          i = 0;
+        } else {
+          arrTop10.splice(k, 1);
+          i = 0;
+        }
       }
     }
-    if (localJukeIdxs.length <= 0) {
-      localJukeIdxs.push(top10[i].startIdx, top10[i].endIdx);
-    }
-    jukeConnectionsIdxs.push(localJukeIdxs);
   }
 
-  jukeConnectionsIdxs.forEach((a) => console.log(a));
+  const topRest = currentDistanceArr
+    .sort((a, b) => a.distance - b.distance)
+    .slice(10);
+
+  console.log(arrTop10);
+  arrTop10.sort((a, b) => b.length - a.length);
+  console.log(arrTop10[0].length * arrTop10[1].length * arrTop10[2].length);
 }
 
-part1();
-// async function part2() {
-//   const content = await Bun.file("./test_day7.txt").text();
-//   const rows = content.split("\n").filter((ifilter) => ifilter !== "");
-//   const rowsSplittedLetters: string[][] = [];
-//
-//   rows.forEach((r) => rowsSplittedLetters.push(r.split("")));
-//
-//   let startingPoint: Point | null = null;
-//   for (let j = 0; j < rowsSplittedLetters[0].length; j++) {
-//     const currLetter = rowsSplittedLetters[0][j];
-//     if (currLetter == "S") {
-//       startingPoint = { row: 0, col: j };
-//       break;
-//     }
-//   }
-//
-//   function recurse(point: Point, memo?: Map<string, number>) {
-//     memo = memo || new Map();
-//
-//     const key1 = `${point.row}-${point.col}`;
-//     if (memo.has(key1)) {
-//       return memo.get(key1);
-//     }
-//
-//     if (point.row > rowsSplittedLetters.length - 1) {
-//       return 1;
-//     }
-//
-//     if (
-//       rowsSplittedLetters[point.row][point.col] == "." ||
-//       rowsSplittedLetters[point.row][point.col] == "S"
-//     ) {
-//       return recurse({ row: point.row + 1, col: point.col }, memo);
-//     } else if (rowsSplittedLetters[point.row][point.col] == "^") {
-//       const val =
-//         recurse({ row: point.row, col: point.col - 1 }, memo) +
-//         recurse({ row: point.row, col: point.col + 1 }, memo);
-//       const key2 = `${point.row}-${point.col}`;
-//       memo.set(key2, val);
-//       return val;
-//     }
-//   }
-//   // console.log(startingPoint);
-//   const counter = recurse(startingPoint!);
-//   console.log(counter);
-// }
-//
-// part2();
+part2();

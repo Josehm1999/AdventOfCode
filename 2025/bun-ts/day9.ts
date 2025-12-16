d31cb2d0e86aabcea5fe6845cf8efcbdd869522a
josehm1999 <josehm1999@gmail.com>
2025-12-16 09:20:42 -0500

    2025 day 9 part 1 completed

---
+++ 2025/bun-ts/day9.ts
@@ -0,0 +1,94 @@
+interface MPoint {
+  x: number;
+  y: number;
+}
+
+async function part1() {
+  const content = await Bun.file("./test_day9.txt").text();
+  const pointArr: MPoint[] = [];
+  content
+    .split("\n")
+    .filter((ifilter) => ifilter !== "")
+    .forEach((x) => {
+      const splits = x.split(",");
+      pointArr.push({ x: parseInt(splits[1]), y: parseInt(splits[0]) });
+    });
+
+  const areaByPairs: Map<string, number> = new Map();
+  for (let i = 0; i < pointArr.length; i++) {
+    for (let j = i + 1; j < pointArr.length; j++) {
+      const key = `${i}-${j}`;
+      if (pointArr[i].x == pointArr[j].x) {
+        areaByPairs.set(
+          key,
+          (pointArr[i].y > pointArr[j].y
+            ? pointArr[i].y - pointArr[j].y
+            : pointArr[j].y - pointArr[i].y) + 2,
+        );
+        continue;
+      }
+
+      if (pointArr[i].y == pointArr[j].y) {
+        areaByPairs.set(
+          key,
+          (pointArr[i].x > pointArr[j].x
+            ? pointArr[i].x - pointArr[j].x
+            : pointArr[j].x - pointArr[i].x) + 2,
+        );
+
+        continue;
+      }
+
+      if (
+        (pointArr[i].x > pointArr[j].x
+          ? pointArr[i].x - pointArr[j].x
+          : pointArr[j].x - pointArr[i].x) ==
+        (pointArr[i].y > pointArr[j].y
+          ? pointArr[i].y - pointArr[j].y
+          : pointArr[j].y - pointArr[i].y)
+      ) {
+        continue;
+      } else {
+        // console.log(
+        //   key,
+        //   pointArr[i],
+        //   pointArr[j],
+        //   ((pointArr[i].x > pointArr[j].x
+        //     ? pointArr[i].x - pointArr[j].x
+        //     : pointArr[j].x - pointArr[i].x) +
+        //     1) *
+        //     ((pointArr[i].y > pointArr[j].y
+        //       ? pointArr[i].y - pointArr[j].y
+        //       : pointArr[j].y - pointArr[i].y) +
+        //       1),
+        // );
+        areaByPairs.set(
+          key,
+          ((pointArr[i].x > pointArr[j].x
+            ? pointArr[i].x - pointArr[j].x
+            : pointArr[j].x - pointArr[i].x) +
+            1) *
+            ((pointArr[i].y > pointArr[j].y
+              ? pointArr[i].y - pointArr[j].y
+              : pointArr[j].y - pointArr[i].y) +
+              1),
+        );
+      }
+    }
+  }
+  let area = 0;
+  for (let val of areaByPairs.values()) {
+    if (area < val) {
+      area = val;
+    }
+  }
+  console.log(area);
+}
+
+part1();
+async function part2() {
+  const content = await Bun.file("./test_day9_example.txt").text();
+  const rows = content.split("\n").filter((ifilter) => ifilter !== "");
+}
+
+// part2();
